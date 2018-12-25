6dadf1c96ff53ee7bfdc8c3427283dea3f3b6b59
604234030cs <604234030@parichat.skru.ac.th>
2018-12-25 11:45:59 +0700

    BMI

---
+++ src/providers/bmi/bmi.ts
@@ -0,0 +1,10 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BmiProvider {
+
+  bmiCalculate(height:number,weight:number){
+    return (weight / height) /height;
+  }
+}
